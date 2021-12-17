72065be090caf029fc0a3cd304021560d79a3329
kuldeep230 <ramdeep214@gmail.com>
2021-12-17 18:16:01 +0530

    remove unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import FilterUI from "./components/FilterUI";
 import axios from "axios";
